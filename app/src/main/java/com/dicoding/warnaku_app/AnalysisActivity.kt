6b065142b5da0deb23b28c3dbb49f2a9ea191500
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-06-06 05:03:23 +0700

    Add Analysis User UI

--- app/src/main/java/com/dicoding/warnaku_app/AnalysisActivity.kt
+++ app/src/main/java/com/dicoding/warnaku_app/AnalysisActivity.kt
@@ -1,20 +1,21 @@
 package com.dicoding.warnaku_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class AnalysisActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_analysis)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonAnalyze: Button = findViewById(R.id.btn_analyze)
+        buttonAnalyze.setOnClickListener {
+            val intent = Intent(this, AnalysisUserActivity::class.java)
+            startActivity(intent)
         }
     }
 }
