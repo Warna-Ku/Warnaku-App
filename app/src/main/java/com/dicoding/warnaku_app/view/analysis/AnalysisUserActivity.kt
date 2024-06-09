eec8beb007d2de57a8e3753b254c53fa6db33ba7
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-06-10 02:29:58 +0700

    adding button function and check the input field

--- app/src/main/java/com/dicoding/warnaku_app/view/analysis/AnalysisUserActivity.kt
+++ app/src/main/java/com/dicoding/warnaku_app/view/analysis/AnalysisUserActivity.kt
@@ -1,21 +1,38 @@
 package com.dicoding.warnaku_app.view.analysis
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.dicoding.warnaku_app.R
+import com.dicoding.warnaku_app.databinding.ActivityAnalysisUserBinding
+import com.dicoding.warnaku_app.view.result.ResultActivity
 
 class AnalysisUserActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAnalysisUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_analysis_user)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityAnalysisUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNext.setOnClickListener {
+            val name = binding.nameEditText.text.toString()
+            val phone = binding.phoneEditText.text.toString()
+            val address = binding.addressEditText.text.toString()
+
+            if (name.isEmpty() || phone.isEmpty() || address.isEmpty()) {
+                AlertDialog.Builder(this).apply {
+                    setTitle("Failed to analysis User")
+                    setMessage("Please fill in all fields")
+                    setPositiveButton("OK") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    create()
+                    show()
+                }
+            } else {
+                val intent = Intent(this, ResultActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
