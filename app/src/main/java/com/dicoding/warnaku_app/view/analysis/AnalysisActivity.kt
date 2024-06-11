e4094f4813fc632afffd4452e775ba2b0a75dfa8
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-06-11 15:27:42 +0700

    add function button open gallery & button analyze

--- app/src/main/java/com/dicoding/warnaku_app/view/analysis/AnalysisActivity.kt
+++ app/src/main/java/com/dicoding/warnaku_app/view/analysis/AnalysisActivity.kt
@@ -1,22 +1,72 @@
 package com.dicoding.warnaku_app.view.analysis
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.dicoding.warnaku_app.R
+import com.dicoding.warnaku_app.databinding.ActivityAnalysisBinding
 
 class AnalysisActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAnalysisBinding
+    private var currentImageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_analysis)
 
-        val buttonAnalyze: Button = findViewById(R.id.btn_analyze)
-        buttonAnalyze.setOnClickListener {
+        binding = ActivityAnalysisBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAnalyze.setOnClickListener {
+            checkImage()
+        }
+
+        binding.btnGallery.setOnClickListener {
+            startGallery()
+        }
+
+        binding.btnCamera.setOnClickListener {
+            startCamera()
+        }
+    }
+
+    private fun checkImage() {
+        if (currentImageUri != null) {
             val intent = Intent(this, AnalysisUserActivity::class.java)
             startActivity(intent)
+        } else {
+            showToast("Please select an image before analyzing.")
         }
+    }
+
+    private fun startCamera() {
+        // TODO: Not yet implemented
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(
+            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+        )
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            binding.imageView.setImageURI(uri)
+            showToast("Image selected: $uri")
+        } else {
+            showToast("No image selected")
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
